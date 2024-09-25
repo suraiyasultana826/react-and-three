5080307f76d850b1b98440976ec38bc867c2470e
suraiyasultana826 <suraiyasultana826@gmail.com>
2024-09-25 21:55:46 +0600

    ..

--- eslint.config.js
+++ eslint.config.js
@@ -22,7 +22,6 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-    
     },
     rules: {
       ...js.configs.recommended.rules,
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
 import './App.css'
 import { useEffect } from 'react';
-import sunTexture from '../src/assets/sun.jpg'
-import Navbar from './Navbar';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 function App() {
   useEffect(() => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
     const canvas = document.getElementById('myThreeJsCanvas');
     const renderer = new THREE.WebGLRenderer({
       canvas,
@@ -17,83 +15,114 @@ function App() {
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    const ambientLight = new THREE.AmbientLight(0x333333);
-    scene.add(ambientLight);
-
-    const textureLoader = new THREE.TextureLoader();
-
-    const solarSystemGroup = new THREE.Group();
-    const earthOrbit = new THREE.Group();
-
-    const sunGeo = new THREE.SphereGeometry(4);
-    const sunMat = new THREE.MeshBasicMaterial({
-      map: textureLoader.load(sunTexture)
-    });
-
-    const sun = new THREE.Mesh(sunGeo, sunMat);
-    solarSystemGroup.add(sun);
-    scene.add(solarSystemGroup);
 
 
-    const earthGeo = new THREE.SphereGeometry(2);
-    const earthMat = new THREE.MeshBasicMaterial({
-      map: textureLoader.load('../src/assets/img/earth.jpg')
-    });
+    const geometry = new THREE.TorusGeometry(.7, .2, 16, 100);
 
-    const earth = new THREE.Mesh(earthGeo, earthMat);
-    earth.position.x=12;
-    earthOrbit.add(earth)
-    scene.add(earthOrbit);
+    const particlesGeometry = new THREE.BufferGeometry;
+    const particlesCnt = 5000;
 
-    const moonOrbit = new THREE.Group();
-    const moonGeo = new THREE.SphereGeometry(1);
-    const moonMat = new THREE.MeshBasicMaterial({
-      map: textureLoader.load('../src/assets/img/pluto.jpg')
-    });
+    const posArray = new Float32Array(particlesCnt * 3);
 
-    const moon = new THREE.Mesh(moonGeo, moonMat);
-    moonOrbit.add(moon);
-    moonOrbit.position.x = 12;
-    moon.position.x = 4;
-    earthOrbit.add(moonOrbit);
+    for (let i =0; i <particlesCnt*3; i++){
+      posArray[i] = (Math.random() - 0.5 )* 10
+    }
+
+    particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3))
+
+    // Materials
+
+    const material = new THREE.PointsMaterial({
+      size:0.005,
+     transparent: true
+    })
+
+    // Mesh
+    const sphere = new THREE.Points(geometry, material)
+    const particleMesh = new THREE.Points(particlesGeometry, material)
+    scene.add(sphere, particleMesh)
+
+    // Lights
+
+    const pointLight = new THREE.PointLight(0xffffff, 0.1)
+    pointLight.position.x = 2
+    pointLight.position.y = 3
+    pointLight.position.z = 4
+    scene.add(pointLight)
 
 
-    //add orbit controls.
-    const controls = new OrbitControls(camera, renderer.domElement);
-    //add FPS stats.
-    const stats = Stats();
-    document.body.appendChild(stats.dom);
+   
 
     camera.position.z = 50;
 
-
-
     function animate() {
 
-      sun.rotation.y += 0.005;
-      earthOrbit.rotation.y += 0.005;
-      moonOrbit.rotation.y += 0.05;
-      stats.update();
-      controls.update();
 
       renderer.render(scene, camera);
-      // window.requestAnimationFrame(animate);
 
     }
     renderer.setAnimationLoop(animate);
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
 
-    window.addEventListener('resize', function () {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    });
+    window.addEventListener('resize', () => {
+      // Update sizes
+      sizes.width = window.innerWidth
+      sizes.height = window.innerHeight
+
+      // Update camera
+      camera.aspect = sizes.width / sizes.height
+      camera.updateProjectionMatrix()
+
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height)
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    })
+
+    camera.position.x = 0
+    camera.position.y = 0
+    camera.position.z = 2
+    // scene.add(camera)
+
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setClearColor(new THREE.Color('#212020'), 1)
+
+    /**
+ * Animate
+ */
+
+    const clock = new THREE.Clock()
+
+    const tick = () => {
+
+      const elapsedTime = clock.getElapsedTime()
+
+      // Update objects
+      sphere.rotation.y = .5 * elapsedTime
+
+      // Update Orbital Controls
+      // controls.update()
+
+      // Render
+      // renderer.render(scene, camera)
+
+      // Call tick again on the next frame
+      window.requestAnimationFrame(tick)
+    }
+
+    tick()
+
+    
+
   }, [])
 
   return (
     <>
-    <Navbar></Navbar>
+      <div>
         <canvas id='myThreeJsCanvas' />
-     
+      </div>
     </>
   )
 }
